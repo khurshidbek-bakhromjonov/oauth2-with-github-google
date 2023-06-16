661906f6f6a6b2e264e16ba68d933a8fbb3029ae
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-06-16 17:55:50 +0500

    done with oauth2 login using google and github

---
+++ src/main/java/com/google/oauth2/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.google.oauth2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+
+    @GetMapping("/account")
+    public String getAccount() {
+        return "Account";
+    }
+}
